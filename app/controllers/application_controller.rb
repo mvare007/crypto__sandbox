8039a04162783a2600abea6006f32675b14474b6
marcovarela <marcovarela@aquaporservicos.pt>
2022-02-13 18:42:40 +0000

    Filtered crypto vs currencies (WIP)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
-	before_action :set_current_vs_currency
+	before_action :set_current_vs_currency, :set_vs_currencies_for_select
 
 	private
 
 	def set_current_vs_currency
 		Current.vs_currency ||= VsCurrency.find_by(symbol: [cookies[:vs_currency] || 'usd'])
+	end
+
+	def set_vs_currencies_for_select
+		filtered_list = %w[ btc eth ltc bch bnb eos xrp xlm link dot yfi bits sats]
+		@vs_currencies_for_select = VsCurrency.all.filter do |currency|
+			filtered_list.exclude?(currency.symbol)
+		end
 	end
 end
